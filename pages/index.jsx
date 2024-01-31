9ca971985b04ce1e526dc7799ffa5682e19f79c5
Hobermac <54035686+Anton1636@users.noreply.github.com>
2024-01-31 21:04:42 +0200

    Added read functions from the smartcontract

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import { generateFakeApartment } from '@/utils/fakeData'
 import { Category, Collection } from '@/components'
+import { getApartments } from '@/services/blockchain'
 
 export default function Home({ apartmentsData }) {
   return (
@@ -17,7 +17,7 @@ export default function Home({ apartmentsData }) {
 }
 
 export const getServerSideProps = async () => {
-  const apartmentsData = generateFakeApartment(5)
+  const apartmentsData = await getApartments()
 
   return {
     props: {
